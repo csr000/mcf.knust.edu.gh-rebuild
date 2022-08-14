7f5619b0db608fee4258ba765a8bc14600c4f367
csr000 <csrichard000@gmail.com>
2022-08-14 01:59:13 +0100

    :art: Improve structure until staff and cohort

--- app/views.py
+++ app/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 
 def index(request):
     return render(request, 'index.html')
 
+
 def about(request):
     return render(request, 'about.html')
 
+
 def register(request):
     return render(request, 'register.html')
+
+
+def event(request):
+    return render(request, 'event.html')
+
+
+def news(request):
+    return render(request, 'news.html')
+
+
+def announcements(request):
+    return render(request, 'announcements.html')
+
+
+def publications(request):
+    return render(request, 'publications.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
