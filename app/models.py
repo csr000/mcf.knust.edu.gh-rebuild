5244d4e5e1df13da028003d1991e97c5be9af62e
Richard Stephen <102833845+csr000@users.noreply.github.com>
2023-05-28 03:42:45 +0100

    minor changes

--- app/models.py
+++ app/models.py
@@ -1,3 +1,122 @@
 from django.db import models
 
-# Create your models here.
+
+class Index(models.Model):
+    logo = models.ImageField()
+    motto = models.CharField(max_length=128)
+    about_program_col1 = models.TextField(verbose_name="About Section - Coloumn 1")
+    about_program_col2 = models.TextField(verbose_name="About Section - Coloumn 2")
+    about_program_col3 = models.TextField(verbose_name="About Section - Coloumn 3")
+    about_program_col4 = models.TextField(verbose_name="About Section - Coloumn 4")
+    our_scholars = models.TextField()
+    how_it_works_url = models.CharField(max_length=128)
+    announcements = models.TextField()
+    copyright = models.CharField(max_length=128)
+
+    class Meta:
+        verbose_name_plural = 'Index'
+
+    def __str__(self):
+        return "Index"
+
+class About(models.Model):
+    tab_title = models.CharField(max_length=64)
+    tab_info = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'About'
+
+
+
+class Cohort(models.Model):
+    img = models.ImageField(upload_to="cohort", verbose_name="Profile Picture")
+    name = models.CharField(max_length=64)
+    programme = models.CharField(max_length=64)
+
+    class Meta:
+        verbose_name_plural = 'Cohort'
+        
+
+
+class CohortBatch(models.Model):
+    batch = models.CharField(max_length=64)
+    cohort = models.ForeignKey(Cohort, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'Cohort Batches'
+
+
+class Register(models.Model):
+    info = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'Register'
+
+    def __str__(self):
+        return "Information About Registration"
+
+
+class Staff(models.Model):
+    img = models.ImageField(upload_to="staff",verbose_name="Profile Picture")
+    name = models.CharField(max_length=64)
+    role = models.CharField(max_length=64)
+
+    class Meta:
+        verbose_name_plural = 'Staff'
+
+
+class Event(models.Model):
+    date_posted = models.DateTimeField()
+    location = models.CharField(max_length=64)
+    title = models.CharField(max_length=128)
+
+    class Meta:
+        verbose_name_plural = 'Event'
+
+
+class News(models.Model):
+    img = models.ImageField(verbose_name="Thumbnail")
+    title = models.CharField(max_length=128)
+    info = models.TextField()
+    date_published = models.DateTimeField()
+
+    class Meta:
+        verbose_name_plural = 'News'
+
+
+class Announcements(models.Model):
+    title = models.CharField(max_length=128)
+    date_published = models.DateTimeField()
+
+    class Meta:
+        verbose_name_plural = 'Announcements'
+
+
+class Publications(models.Model):
+    title = models.CharField(max_length=64)
+    url_to_file = models.CharField(max_length=128)
+
+    class Meta:
+        verbose_name_plural = 'Publications'
+
+
+class Contact(models.Model):
+    telephone = models.CharField(max_length=16)
+    cell_phone = models.CharField(max_length=16)
+    address = models.TextField(max_length=256)
+    email1 = models.CharField(max_length=32, verbose_name="first email")
+    email2 = models.CharField(max_length=32, verbose_name="second email")
+    map = models.TextField(max_length=128)
+
+    class Meta:
+        verbose_name_plural = 'Contact'
+
+
+class Socials(models.Model):
+    facebook = models.CharField(max_length=128)
+    twitter = models.CharField(max_length=128)
+    instagram = models.CharField(max_length=128)
+    youtube = models.CharField(max_length=128)
+
+    class Meta:
+        verbose_name_plural = 'Socials'
--- app/views.py
+++ app/views.py
@@ -1,55 +1,72 @@
 from django.shortcuts import render
+from .models import Index, About, CohortBatch, Register, Staff, Event, News, Announcements, Publications, Contact, Socials
 
 
-def index(request):
-    return render(request, 'index.html')
+def index(request, context={}):
+    context['data'] = Index.objects.all()[0]
+    return render(request, 'index.html', context)
 
 
 def apply(request):
     return render(request, 'apply.html')
 
 
-def about(request):
-    return render(request, 'about.html')
+def about(request, context={}):
+    context['data'] = About.objects.all()
+    return render(request, 'about.html', context)
 
 
-def register(request):
-    return render(request, 'register.html')
+def register(request, context={}):
+    context['data'] = Register.objects.get(pk=1)
+    return render(request, 'register.html', context)
 
 
-def staff(request):
-    return render(request, 'staff.html')
+def staff(request, context={}):
+    context['data'] = Staff.objects.all()
+    return render(request, 'staff.html', context)
 
 
-def event(request):
-    return render(request, 'event.html')
+def event(request, context={}):
+    context['data'] = Event.objects.all()
+    return render(request, 'event.html', context)
 
 
-def news(request):
-    return render(request, 'news.html')
+def news(request, context={}):
+    context['data'] = News.objects.all()
+    return render(request, 'news.html', context)
 
 
-def announcements(request):
-    return render(request, 'announcements.html')
+def announcements(request, context={}):
+    context['data'] = Announcements.objects.all()
+    return render(request, 'announcements.html', context)
 
 
-def publications(request):
-    return render(request, 'publications.html')
+def publications(request, context={}):
+    context['data'] = Publications.objects.all()
+    return render(request, 'publications.html', context)
 
 
-def contact(request):
-    return render(request, 'contact.html')
+def contact(request, context={}):
+    context['data'] = Contact.objects.get(pk=1)
+    context['socials'] = Socials.objects.get(pk=1)
+    return render(request, 'contact.html', context)
 
 
-def cohort(request, id):
+def cohort(request, id, context={}):
+    context['data'] = CohortBatch.objects.all()
     match id:
         case '1':
-            return render(request, 'cohort_one.html')
+            context['data'] = context['data'].filter(batch='2014/2015 Academic Year')
+            return render(request, 'cohort_one.html', context)
         case '2':
-            return render(request, 'cohort_two.html')
+            context['data'] = context['data'].filter(batch='2015/2016 Academic Year')
+            return render(request, 'cohort_two.html', context)
         case '3':
-            return render(request, 'cohort_three.html')
+            context['data'] = context['data'].filter(batch='2016/2017 Academic Year')
+            return render(request, 'cohort_three.html', context)
         case '4':
-            return render(request, 'cohort_four.html')
+            context['data'] = context['data'].filter(batch='2017/2018 Academic Year')
+            return render(request, 'cohort_four.html', context)
         case '5':
-            return render(request, 'cohort_five.html')
+            context['data'] = context['data'].filter(batch='2018/2019 Academic Year')
+            return render(request, 'cohort_five.html', context)
